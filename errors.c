b1876d28596ddc420e9c8b30e63c954e1c2576ef
MMhenna <mhenna.mehdi@gmail.com>
2021-05-13 11:36:32 +0100

    break

--- errors.c
+++ errors.c
@@ -24,27 +24,32 @@ void err(int error_code, ...)
 	{
 		case 1:
 			fprintf(stderr, "USAGE: monty file\n");
+			break;
 		case 2:
-			fprintf(stderr, "Error: Can't open file %s\n",
-				va_arg(ag, char *));
+			fprintf(stderr, "Error: Can't open file %s\n", va_arg(ag, char *));
+			break;
 		case 3:
 			l_num = va_arg(ag, int);
 			op = va_arg(ag, char *);
 			fprintf(stderr, "L%d: unknown instruction %s\n", l_num, op);
+			break;
 		case 4:
 			fprintf(stderr, "Error: malloc failed\n");
+			break;
 		case 5:
 			fprintf(stderr, "L%d: usage: push integer\n", va_arg(ag, int));
+			break;
 		case 6:
-			fprintf(stderr, "L%d: can't pint, stack empty\n",
-				va_arg(ag, int));
+			fprintf(stderr, "L%d: can't pint, stack empty\n", va_arg(ag, int));
+			break;
 		case 7:
-			fprintf(stderr, "L%d: can't pop an empty stack\n",
-				va_arg(ag, int));
+			fprintf(stderr, "L%d: can't pop an empty stack\n", va_arg(ag, int));
+			break;
 		case 8:
 			l_num = va_arg(ag, int);
 			op = va_arg(ag, char *);
 			fprintf(stderr, "L%d: can't %s, stack too short\n", l_num, op);
+			break;
 		default:
 			break;
 	}
